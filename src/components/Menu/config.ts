0ac93aa3aa71a5a5a1652ac89286bf89ff591392
mummyfinance <financemummy@gmail.com>
2021-02-25 02:40:56 -0300

    Mummy Frontend AMM

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,86 @@
+import { MenuEntry } from '@pancakeswap-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: 'https://mummy.finance/'
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    initialOpenState: true,
+    items: [
+      {
+        label: 'Exchange',
+        href: '/swap'
+      },
+      {
+        label: 'Liquidity',
+        href: '/pool'
+      }
+    ]
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: 'https://mummy.finance/farms'
+  },
+  {
+    label: 'Sand Pools',
+    icon: 'PoolIcon',
+    href: 'https://mummy.finance/sandpools',
+  },
+  // {
+  //   label: 'Lottery',
+  //   icon: 'TicketIcon',
+  //   href: 'https://www.goosedefi.com/lottery'
+  // },
+  {
+    label: 'Info',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: 'PancakeSwap',
+        href: 'https://pancakeswap.info/token/0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
+      },
+      /* {
+        label: 'CoinGecko',
+        href: 'https://www.coingecko.com/en/coins/goose-finance',
+      },
+      {
+        label: 'CoinMarketCap',
+        href: 'https://coinmarketcap.com/currencies/goose-finance/',
+      },
+      {
+        label: 'AstroTools',
+        href: 'https://app.astrotools.io/pancake-pair-explorer/0x19e7cbecdd23a16dfa5573df54d98f7caae03019',
+      }, */
+    ]
+  },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: "Github",
+        href: "https://github.com/mummyfinance/",
+      },
+      {
+        label: "Docs",
+        href: "",
+      },
+      {
+        label: "Blog",
+        href: "",
+      },
+    ],
+  },
+  /* {
+    label: 'Audit by Hacken',
+    icon: 'AuditIcon',
+    href: 'https://www.goosedefi.com/files/hackenAudit.pdf',
+  }, */
+]
+
+export default config
