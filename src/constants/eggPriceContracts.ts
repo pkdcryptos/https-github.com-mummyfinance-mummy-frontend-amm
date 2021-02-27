894de353c3aed7fee78ffbb942368171489b45c4
mummyfinance <financemummy@gmail.com>
2021-02-27 15:04:57 -0300

    Token Address & Medium link

--- src/constants/eggPriceContracts.ts
+++ src/constants/eggPriceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
-  cakeAddress: '0xf952fc3ca7325cc27d15885d37117676d25bfda6', /* Token address */
+  cakeAddress: '0x9c5D0C6a3be22B5dbd18275527CC17FB6327d696', /* Token address */
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-  lpAddress: '0x19e7cbecdd23a16dfa5573df54d98f7caae03019'
+  lpAddress: '0x5824bbe60efd1b321abb3975f07388d898da5c9f'
 }
 
 export default priceContracts
